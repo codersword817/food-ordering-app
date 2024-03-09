761bb997ff37858e5bfe18c4e8f4f13270759d69
aakash <marojuaakash2003@gmail.com>
2024-03-09 23:12:05 +0530

    error page

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -2,7 +2,8 @@ const Error = () => {
   return (
     <>
       <div className="error-container">
-        <h1>HEyy THis is an Invalid Route pls Check </h1>
+        <h1>Heyy This is an Invalid Route pls Check </h1>
+        <h2>Error!!!</h2>
       </div>
     </>
   );
